07f89d3fe87d4baf5af3a32350edfc6d14eeb989
daimatz <dai@daimatz.net>
2015-03-02 10:56:20 +0900

    install libs for scala

--- itamae/recipes/scala.rb
+++ itamae/recipes/scala.rb
@@ -2,6 +2,15 @@
 include_recipe 'base.rb'
 include_recipe 'java.rb'
 
+[
+  'libjansi-java',
+  'libjansi-native-java',
+  'libhawtjni-runtime-java',
+
+].each do |pkg|
+  package pkg
+end
+
 {
   scala: 'http://www.scala-lang.org/files/archive/scala-2.10.4.deb',
   sbt: 'http://dl.bintray.com/sbt/debian/sbt-0.13.6.deb',
