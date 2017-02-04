cab2881e10b72bb3e77db27cb4a818e9eac1d2e5
daimatz <dai@daimatz.net>
2017-02-05 03:29:03 +0900

    update intellij

--- itamae/recipes/intellij.rb
+++ itamae/recipes/intellij.rb
@@ -1,15 +1,8 @@
 
 include_recipe 'base.rb'
 
-[
-  'xfce4',
-  'fonts-vlgothic',
-].each do |pkg|
-  package pkg
-end
-
 execute "download & unarchive" do
-  url = 'http://download.jetbrains.com/idea/ideaIC-14.1.4.tar.gz'
+  url = 'https://download.jetbrains.com/idea/ideaIC-2016.3.4-no-jdk.tar.gz'
   command "curl -L #{url} | tar -xz -C #{node[:home]} && mv #{node[:home]}/idea-IC-* #{node[:home]}/intellij"
   not_if "test -d #{node[:home]}/intellij"
 end
