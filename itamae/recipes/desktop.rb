7f7261bd14c7cda6bb8c812eb3ed64e3f74f10ff
daimatz <dai@daimatz.net>
2017-02-05 03:22:43 +0900

    add desktop for ubuntu-1604

---
+++ itamae/recipes/desktop.rb
@@ -0,0 +1,15 @@
+
+include_recipe 'base.rb'
+
+[
+  'xfce4',
+  'fonts-vlgothic',
+  'xserver-xorg-legacy',
+].each do |pkg|
+  package pkg
+end
+
+execute "set needs_root_rights" do
+  user 'root'
+  command 'echo "needs_root_rights=yes\nallowed_users=anybody" > /etc/X11/Xwrapper.config'
+end
