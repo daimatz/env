95c04ee5c4558ae3e1f9fbe0242511fcbd69e337
daimatz <dai@daimatz.net>
2020-01-04 00:13:44 +0900

    update protobuf

--- itamae/extra/protobuf.rb
+++ itamae/extra/protobuf.rb
@@ -1,18 +1,18 @@
 execute 'protobuf' do
-  version = '2.6.1'
-  dir = "protobuf-#{version}"
-  tgz = "#{dir}.tar.gz"
-  url = "https://github.com/google/protobuf/releases/download/v#{version}/#{tgz}"
+  version = '3.11.2'
+  dir = "protoc-#{version}-linux-x86_64"
+  zip = "#{dir}.zip"
+  url = "https://github.com/protocolbuffers/protobuf/releases/download/v#{version}/#{zip}"
 
   not_if 'which protoc'
 
   user 'root'
   command <<-CMD
-cd /tmp
-wget #{url} -O #{tgz}
-tar xf #{tgz}
-cd #{dir}
-./configure && make && make install
-ldconfig
+cd /tmp/$(mktemp XXXXX)
+wget #{url} -O #{zip}
+unzip #{zip}
+mkdir -p /usr/local/include/google/protobuf/ /usr/local/bin/
+cp -r include/google/protobuf/* /usr/local/include/google/protobuf/
+cp bin/protoc /usr/local/bin/
   CMD
 end
