b3f1c50e8d212af6776a04af52fc6ca9ea35bc49
daimatz <dai@daimatz.net>
2021-04-20 01:22:17 +0900

    install openjdk-11-jdk

--- itamae/base/004_java.rb
+++ itamae/base/004_java.rb
@@ -1,11 +1,11 @@
 execute 'add apt repository' do
   user 'root'
   command 'add-apt-repository -y ppa:openjdk-r/ppa && apt update'
-  not_if 'apt search openjdk-8-jdk | grep openjdk'
+  not_if 'apt search openjdk-11-jdk | grep openjdk'
 end
 
 [
-  'openjdk-8-jdk',
+  'openjdk-11-jdk',
   'maven',
 ].each do |pkg|
   package pkg
