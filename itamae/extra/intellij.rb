09fc43569659725fd869f82e2286b6901ba0d0d0
daimatz <dai@daimatz.net>
2023-03-09 02:58:36 +0900

    update IDEA

--- itamae/extra/intellij.rb
+++ itamae/extra/intellij.rb
@@ -1,5 +1,5 @@
 execute "download & unarchive" do
-  url = 'https://download.jetbrains.com/idea/ideaIC-2016.3.4-no-jdk.tar.gz'
+  url = 'https://download.jetbrains.com/idea/ideaIC-2022.3.2.tar.gz'
   command "curl -L #{url} | tar -xz -C #{node[:home]} && mv #{node[:home]}/idea-IC-* #{node[:home]}/intellij"
   not_if "test -d #{node[:home]}/intellij"
 end
