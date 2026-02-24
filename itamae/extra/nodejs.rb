20afec5b688998a538b7cb0b20e2205f1a5fc831
daimatz <dai@daimatz.net>
2026-02-25 02:32:46 +0900

    update nodejs

--- itamae/extra/nodejs.rb
+++ itamae/extra/nodejs.rb
@@ -1,15 +1,4 @@
 execute 'install nodebrew' do
-  user node[:name]
-  command 'curl -sL git.io/nodebrew | perl - setup'
-  not_if 'which nodebrew'
-end
-
-execute 'install nodejs' do
-  version = '8.10.0'
-  user node[:name]
-  command <<-CMD
-#{node[:home]}/.nodebrew/current/bin/nodebrew install-binary #{version}
-#{node[:home]}/.nodebrew/current/bin/nodebrew use #{version}
-#{node[:home]}/.nodebrew/current/bin/nodebrew alias default #{version}
-  CMD
+  command 'curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s install stable'
+  not_if 'which n'
 end
