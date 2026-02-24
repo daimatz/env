a11dbc23262bf70a9f913cf50d6a776052383716
daimatz <dai@daimatz.net>
2026-02-25 02:33:44 +0900

    add homebrew

---
+++ itamae/extra/homebrew.rb
@@ -0,0 +1,4 @@
+execute 'install homebrew' do
+  command '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+  not_if 'which brew'
+end
--- itamae/extra/nodejs.rb
+++ itamae/extra/nodejs.rb
@@ -1,4 +1,4 @@
-execute 'install nodebrew' do
+execute 'install nodejs' do
   command 'curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s install stable'
   not_if 'which n'
 end
