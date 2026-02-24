58395378ee7261dbaff30c59234abe8b69b5c3c8
daimatz <dai@daimatz.net>
2026-02-25 02:45:52 +0900

    fix user

--- itamae/extra/homebrew.rb
+++ itamae/extra/homebrew.rb
@@ -1,4 +1,5 @@
 execute 'install homebrew' do
+  user node[:name]
   command '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
   not_if 'which brew'
 end
