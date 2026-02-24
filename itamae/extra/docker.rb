1dcd415277a56be87901856be0b617b034f724a5
daimatz <dai@daimatz.net>
2026-02-25 02:32:56 +0900

    add docker

---
+++ itamae/extra/docker.rb
@@ -0,0 +1,22 @@
+execute 'install nodebrew' do
+  command <<-CMD
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  CMD
+  not_if 'which docker'
+end
