cd10d20b3385c3e40b4ecd8a623fb72e0ecaec57
daimatz <dai@daimatz.net>
2026-02-25 02:48:52 +0900

    noninteractive

--- itamae/extra/homebrew.rb
+++ itamae/extra/homebrew.rb
@@ -1,5 +1,5 @@
 execute 'install homebrew' do
   user node[:name]
-  command '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+  command 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
   not_if 'which brew'
 end
